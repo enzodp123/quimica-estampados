0939c14229c776b02f780d7dbc0b321c1e9a8a9c
enzo <enzopazwork@gmail.com>
2026-08-12 16:26:10 -0300

    sections v0.1: Add new images to assets: img2.png and img3.png

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        lucide: ['badge-check', 'truck', 'layers-3', 'headphones', 'store'],
+      },
+    }),
+  ],
+  devToolbar: {
+    enabled: false
+  }
+});
